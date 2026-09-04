bf3c0c5c9c08b9def052e5d0de6729158a6e93a3
lucas <lucasrangel2011@gmail.com>
2026-09-03 22:05:33 -0300

    docker update

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-if [ -f /output/csv/patients.csv ] || [ -f /output/patients.csv ]; then
-  echo "Synthea data already exists in /output. Skipping generation..."
-  exit 0
-else
-  echo "No existing synthea data found. Generating fresh biological data..."
-  ./run_synthea -p 100 --exporter.csv.export=true --exporter.baseDirectory=/output
-fi
