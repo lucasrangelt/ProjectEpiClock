ca4b05a84204c87aa02f5bb10d9dbee81e2aa01a
lucas <lucasrangel2011@gmail.com>
2026-09-11 01:43:28 +0000

    added horvarth's file

--- src/ingest_synthea.py
+++ src/ingest_synthea.py
@@ -1,11 +1,13 @@
 import duckdb
 
-my_connection = duckdb.connect()
-df_patients = my_connection.execute("""
-    SELECT
-        id AS patient_id,
-        gender,
-        DATE_DIFF('year', CAST(birthday AS DATE), CURRENT_DATE) AS chronological_age
-    FROM
-        './data/csv/patients.csv'
-""").df()
+with duckdb.connect() as my_connection:
+    df_patients = my_connection.execute("""
+        SELECT
+            id AS patient_id,
+            gender,
+            DATE_DIFF('year', CAST(BIRTHDATE AS DATE), CURRENT_DATE) AS chronological_age
+        FROM
+            './data/csv/patients.csv'
+    """).df()
+    
+print(df_patients.head())
